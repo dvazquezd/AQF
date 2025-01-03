1c1cbf54ee4261a1d2fe423cd51e1493c7b3a7fc
dvd <davazquezd@gmail.com>
2025-01-03 19:36:36 +0100

    Refactor finisehd

--- lib/DataLoader.py
+++ lib/DataLoader.py
@@ -1,15 +1,31 @@
 import json
 import pandas as pd
-from lib.ApiClient import ApiClient
-import lib.DataTransformer as transf
-import lib.BricMortar as bm
+import DataTransformer as transf
+import BricMortar as bm
+
+
+def combine_dataframes(df_historical,df_current,f_dataframes, combine_configuration):
+   '''
+   Combine same datasets types deleting duplicates
+   '''
+   all_keys = df_current.keys()
+
+   for enconomic in ['unemployment','nonfarm_payroll','cpi']:
+       f_dataframes[enconomic] = df_current[enconomic]
+       all_keys.remove(enconomic)
+       
+   for key in all_keys:
+       df_combined = pd.concat([df_historical[key], df_current[key]]).drop_duplicates(subset=combine_configuration[key], keep='last')
+       f_dataframes[key] = df_combined   
+   
+   return f_dataframes
 
 
 def combine_data(df_historical,df_current,subset_columns):
    '''
-   Combine same datasets types deleting duplicates
+        Combine same datasets types deleting duplicates
    '''
-   df_combined = pd.concat([df_historical, df_current]).drop_duplicates(subset=subset_columns, keep="last")
+   df_combined = pd.concat([df_historical, df_current]).drop_duplicates(subset=subset_columns, keep='last')
    return df_combined
 
 
@@ -51,11 +67,11 @@ def load_economics(dfs, client, indicators):
         json_data = client.get_economic_indicator(indicator)
         if json_data:
             df_economic = transf.transform_economic_data(json_data)
-            bm.write_csv(df_economic, f"data/df_{indicator}.csv")
+            bm.write_csv(df_economic, f'data/df_{indicator}.csv')
             dfs[indicator] = df_economic
             print(f'Getting data: {{\'function\': {indicator}}} saved successfully!')
         else:
-            print(f"Error fetching data for {indicator}")
+            print(f'Error fetching data for {indicator}')
     return dfs 
 
 
@@ -123,84 +139,10 @@ def save_dataframes(dfs):
         bm.write_csv(dfs[df],f'data/df_{df}.csv')
 
 
-def main():
+def load_config():
     '''
     '''
-    # Variables and configurations
-    historical_needed = True
-    symbols = ['NVDA']
-    periods = [20, 40, 200]
-    #technical_indicators = ['macd','rsi','sma']
-    economic_indicators = ['unemployment', 'nonfarm_payroll', 'cpi']
-    #all_dfs = ['df_macd','df_rsi','df_sma','df_ticker','df_news']
-    topics = ['technology']#,'blockchain','financial_markets','economy_macro','economy_monetary','economy_fiscal']
-    dataframes = {'ticker': pd.DataFrame(),
-                  'macd': pd.DataFrame(),
-                  'rsi': pd.DataFrame(),
-                  'rsi_20': pd.DataFrame(),
-                  'rsi_40': pd.DataFrame(),
-                  'rsi_200': pd.DataFrame(),
-                  'sma': pd.DataFrame(),
-                  'sma_20': pd.DataFrame(),
-                  'sma_40': pd.DataFrame(),
-                  'sma_200': pd.DataFrame(),
-                  'unemployment': pd.DataFrame(),
-                  'cpi': pd.DataFrame(),
-                  'nonfarm_payroll': pd.DataFrame(),
-                  'news': pd.DataFrame(),
-                  'merged_tec_info': pd.DataFrame()}
-    h_dataframes = dataframes
-    tec_columns = {
-        'macd': ['ticker', 'datetime', 'MACD', 'MACD_Signal', 'MACD_Hist'],
-        'rsi_200': ['ticker', 'datetime', 'rsi_200'],
-        'rsi_40': ['ticker', 'datetime', 'rsi_40'],
-        'rsi_20': ['ticker', 'datetime', 'rsi_20'],
-        'sma_200': ['ticker', 'datetime', 'sma_200'],
-        'sma_40': ['ticker', 'datetime', 'sma_40'],
-        'sma_20': ['ticker', 'datetime', 'sma_20']
-    }
-    economic_columns = {
-        'cpi': 'cpi',
-        'nonfarm_payroll': 'nonfarm',
-        'unemployment': 'unemployment'
-    }
-    
+    with open('../config/config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-    #Objects
-    client = ApiClient()
-
-    months = bm.get_months(2024, historical_needed)
-    dataframes = load_data(dataframes,client, symbols, months, periods)
-    dataframes = load_economics(dataframes, client, economic_indicators)
-    dataframes = load_news(dataframes, client, months, topics)
-    dataframes = merge_datasets(dataframes, periods, tec_columns, economic_columns)
-
-    if not historical_needed:
-        h_dataframes = retrieve_data(h_dataframes)
-        #dataframes = combine_data(dataframes, h_dataframes)
-    
-    print(dataframes)
-    save_dataframes(dataframes)
-
-
-
-    '''  
-        df_ticker = combine_data(df_ticker, dh_ticker, subset_columns=['ticker','datetime'])
-        df_macd = combine_data(df_macd, dh_mac, subset_columns=['ticker','datetime'])
-        df_sma = combine_data(df_sma, dh_sma, subset_columns=['ticker','datetime','period'])
-        df_rsi = combine_data(df_rsi, dh_rsi, subset_columns=['ticker','datetime','period'])
-        df_news = combine_data(df_news, dh_news, subset_columns=['title','time_published','ticker','affected_topic'])
-
-        bm.write_csv(df_ticker,'data/df_ticker.csv')
-        bm.write_csv(df_macd,'data/df_macd.csv')
-        bm.write_csv(df_sma,'data/df_sma.csv')
-        bm.write_csv(df_rsi,'data/df_rsi.csv')
-        bm.write_csv(df_news,'data/df_news.csv')
-
-        df_aqf = merge_datasets(df_ticker, df_macd, df_rsi, df_sma, df_unemployment, df_nonfam, df_cpi)
-        bm.write_csv(df_aqf,'data/df_aqf.csv')
-    '''
-
-
-if __name__ == "__main__":
-    main()
+    return config
