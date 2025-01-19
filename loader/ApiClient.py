ac4eb76b390e6dc75c3e9e45d452262538057f5c
dvd <davazquezd@gmail.com>
2025-01-19 09:52:12 +0100

    Uploading code refactor

--- loader/ApiClient.py
+++ loader/ApiClient.py
@@ -9,7 +9,6 @@ class ApiClient:
         self.baseUrl = 'https://www.alphavantage.co/query'
         self.requests_made = []
 
-
     def _control_rate_limit(self):
         """
         Controla el número de peticiones por minuto para evitar sobrepasar el límite.
@@ -23,7 +22,6 @@ class ApiClient:
             time.sleep(sleep_time)  # Pausar hasta que podamos hacer más peticiones
 
         self.requests_made.append(current_time)  # Añadir el timestamp de la nueva petición
-
 
     def _get(self, params):
         """
@@ -41,7 +39,6 @@ class ApiClient:
         except requests.exceptions.RequestException as e:
             print(f"Request failed: {e}")
             return None
-
 
     def get_data(self, function, symbol, **kwargs):
         """
@@ -70,8 +67,7 @@ class ApiClient:
         else:
             print('Error o datos no encontrados en la respuesta')
             return None
-        
-
+ 
     def get_economic_indicator(self, function):
         """
         Obtiene un indicador económico como unemployment, CPI, inflation, etc.
@@ -81,31 +77,26 @@ class ApiClient:
         }
         return self._get(params)
     
-
     # Métodos para funciones específicas
     def get_intraday_data(self, symbol, month=None, interval='60min', outputsize='full', entitlement ='delayed', extended_hours='true'):
         """
         """
         return self.get_data('TIME_SERIES_INTRADAY', symbol, interval=interval, outputsize=outputsize, entitlement=entitlement, extended_hours=extended_hours, month=month)
 
-
     def get_sma(self, symbol, month=None, time_period=200, interval='60min', series_type='close', entitlement ='delayed', extended_hours='true'):
         """
         """
         return self.get_data('SMA', symbol, time_period=time_period, interval=interval, series_type=series_type, entitlement=entitlement, extended_hours=extended_hours, month=month)
-
 
     def get_macd(self, symbol, month=None, interval='60min', series_type='close', entitlement ='delayed', extended_hours='true'):
         """
         """
         return self.get_data('MACD', symbol, interval=interval, series_type=series_type, entitlement=entitlement, extended_hours=extended_hours, month=month)
 
-
     def get_rsi(self, symbol, month=None, time_period=200, interval='60min', series_type='close',  entitlement ='delayed', extended_hours='true'):
         """
         """
         return self.get_data('RSI', symbol, time_period=time_period, interval=interval, series_type=series_type, entitlement=entitlement, extended_hours=extended_hours, month=month)
-
 
     def get_news_sentiment(self, topics='economy_macro', time_from='20240410T0130', time_to='20240415T0130', limit=1000, sort='RELEVANCE'):
         """
